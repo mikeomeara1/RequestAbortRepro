d577cc66700657f760e1efe44d167719c690d5ab
Mike O'Meara <momeara@decisioninsite.com>
2018-04-30 18:12:09 -0700

    Add project files.

---
+++ RequestAbortRepro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RequestAbortRepro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
